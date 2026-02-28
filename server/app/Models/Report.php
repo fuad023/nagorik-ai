e527fe895f6f4350bcbc4873aed9931b07ebae76
fuad <nafisfuadisc@gmail.com>
2026-03-01 03:57:57 +0600

    user and report table initialized

---
+++ server/app/Models/Report.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Report extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'author_id'
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
