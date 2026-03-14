8d3bc8e08ab78c81d97e8a5c1e8543c5ab1e8bab
fuad <nafisfuadisc@gmail.com>
2026-03-14 16:58:59 +0600

    refactor: user related code

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,11 +10,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
@@ -23,21 +17,11 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -45,9 +29,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    // public function posts(): HasMany
-    // {
-    //     return $this->hasMany(Post::class);
-    // }
 }
